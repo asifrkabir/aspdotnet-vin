dbcb73e33e4dac08aaf8d4b7529e08f3aeba2fe7
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-09-11 20:49:44 +0600

    UI completed

---
+++ session-6/ERPSolution/HRISWebApplication/Setup/OfficeLocationForm.aspx.cs
@@ -0,0 +1,34 @@
+﻿using HRISWebApplication.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HRISWebApplication.Setup
+{
+    public partial class OfficeLocationForm : System.Web.UI.Page
+    {
+        private CompanyDataAccess companyDataAccess;
+        public OfficeLocationForm()
+        {
+            companyDataAccess = new CompanyDataAccess();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSaveOfficeLocation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClearOfficeLocation_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
